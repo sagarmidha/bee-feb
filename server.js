16291637df86147492c4e27c83a4566c3197774a
sagar <sagar1197.be23@chitkara.edu.in>
2025-02-20 16:55:43 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/signup",(req,res)=>{
+    res.sendFile(__dirname+"/signup.html")
+})
+app.post("/signup",(req,res)=>{
+   let{username,useremail,userpassword}=req.body;
+   console.log(username,useremail,userpassword);
+})
+
+app.listen(3434,()=>{
+    console.log("Server is running on port ");
+})
