136a7349d566c2800dc13ba906af6a56f84ad055
sagar <sagar1197.be23@chitkara.edu.in>
2025-02-21 19:36:28 +0530

    commit on 21feb

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
-
+app.use(express.urlencoded({extended:true}))
+let userdata=[]
 app.get("/signup",(req,res)=>{
     res.sendFile(__dirname+"/signup.html")
 })
 app.post("/signup",(req,res)=>{
    let{username,useremail,userpassword}=req.body;
-   console.log(username,useremail,userpassword);
+   let newuser={
+     name:username,
+     email:useremail,
+     password:userpassword
+   }
+   userdata.push(newuser);
+   //console.log(username,useremail,userpassword);
+   res.send(userdata);
 })
 
 app.listen(3434,()=>{
